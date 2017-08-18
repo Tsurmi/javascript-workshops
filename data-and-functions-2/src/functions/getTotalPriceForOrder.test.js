109632d038793f49ed213b1530f00d2aad8cb766
Jay Cheroske <jay@portal.org>
2017-08-17 19:29:16 -0700

    getTotalPriceForOrder tests

--- data-and-functions-2/src/functions/getTotalPriceForOrder.test.js
+++ data-and-functions-2/src/functions/getTotalPriceForOrder.test.js
@@ -9,5 +9,39 @@ describe('getTotalPriceForOrder', () => {
       expect(typeof getTotalPriceForOrder).toBe('function')
   )
 
-  it()
+  it(
+    'throws with no data',
+    () =>
+      expect(() => getTotalPriceForOrder(null, 1)).toThrow(Error)
+  )
+
+  it(
+    'throws with undefined orders',
+    () =>
+      expect(() => getTotalPriceForOrder({}, 1)).toThrow(Error)
+  )
+
+  it(
+    'throws with no id',
+    () =>
+      expect(() => getTotalPriceForOrder(DATA, undefined)).toThrow(Error)
+  )
+
+  it(
+    'returns null if the order is not found',
+    () =>
+      expect(getTotalPriceForOrder(DATA, 100)).toBeNull()
+  )
+
+  it(
+    'returns a number',
+    () =>
+      expect(typeof getTotalPriceForOrder(DATA, 1)).toBe('number')
+  )
+
+  it(
+    'returns the correct result',
+    () =>
+      expect(getTotalPriceForOrder(DATA, 3)).toBe(75)
+  )
 })
