2322d055631025f667e37de52e8acf0205e33543
Jay Cheroske <jay@portal.org>
2017-08-17 18:10:53 -0700

    Copied in workshop 2

---
+++ data-and-functions-2/src/functions/getTotalPriceForOrder.test.js
@@ -0,0 +1,13 @@
+/* global describe, expect, it */
+import DATA from '../DATA'
+import getTotalPriceForOrder from './getTotalPriceForOrder'
+
+describe('getTotalPriceForOrder', () => {
+  it(
+    'is a function',
+    () =>
+      expect(typeof getTotalPriceForOrder).toBe('function')
+  )
+
+  it()
+})
