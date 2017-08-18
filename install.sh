5cf42ca94c259150ea77d21631a4ced15999eb7f
Jay Cheroske <jay@portal.org>
2017-08-18 11:56:03 -0700

    tweaks

--- install.sh
+++ install.sh
@@ -3,8 +3,7 @@
 git remote add upstream https://github.com/AmericaCampaign/javascript-workshops.git
 
 DIR=`dirname "$0"`
-cd ${DIR} && npm install
-cd ./data-and-functions-1 && npm install
+cd ${DIR}/data-and-functions-1 && npm install
 cd ../data-and-functions-2 && npm install
 cd ../data-and-functions-3 && npm install
 cd ..
