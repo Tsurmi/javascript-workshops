4d77ff711074af65755a630f2dc076fc7ae82a2f
Jay Cheroske <jay@portal.org>
2017-08-20 13:17:38 -0700

    addProduct finished

--- data-and-functions-3/src/functions/addProduct.js
+++ data-and-functions-3/src/functions/addProduct.js
@@ -2,4 +2,12 @@ export default (data, newProduct) => {
   if (!data || !data.products || !newProduct) {
     throw new Error('Invalid parameters')
   }
+
+  if (newProduct.id || !newProduct.name || !newProduct.price) {
+    throw new Error('Bad product param', newProduct)
+  }
+
+  newProduct.id = data.products.reduce((maxId, p) => Math.max(maxId, p.id), 0) + 1
+  data.products.push(newProduct)
+  return newProduct
 }
