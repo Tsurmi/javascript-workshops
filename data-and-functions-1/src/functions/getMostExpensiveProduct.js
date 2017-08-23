6a6c52ef0aca828ecdf8f33a00ce407e67e6ef11
Travis Surmi <surmi.travis@gmail.com>
2017-08-23 09:52:50 -0700

    most expenisive

--- data-and-functions-1/src/functions/getMostExpensiveProduct.js
+++ data-and-functions-1/src/functions/getMostExpensiveProduct.js
@@ -1,15 +1,16 @@
-const getMostExpensiveProduct = (data, products) => {
+const getMostExpensiveProduct = (data) => {
   if (data == null || data.products == null) {
     return null
-  } else {
-    let mostExpensive = data.products[0]
-    for (let i = 0; i < data.products.length; i++) {
-      if (mostExpensive.price < products.price) {
-        mostExpensive = products
-      }
-    }
-    return mostExpensive
   }
+
+  let mostExpensive = data.products[0]
+  for (let i = 1; i < data.products.length; i++) {
+    const currentProduct = data.products[i]
+    if (mostExpensive.price < currentProduct.price) {
+      mostExpensive = currentProduct
+    }
+  }
+  return mostExpensive
 }
 
 export default getMostExpensiveProduct
